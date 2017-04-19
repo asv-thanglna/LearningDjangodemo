2858660d3a56e7be1fbf7496392dfabc1bcd9623
thanglna <thanglna@aoi-sys.vn>
2017-04-19 15:37:45 +0700

    dev2: test commit hdjks

---
+++ myapp/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+class Person (models.Model):
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+
+	@property
+	def full_name(self):
+		return '%s %s' % (self.first_name, self.last_name)
+
+
+class Musician (models.Model):
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	instrument = models.CharField(max_length=100)
+
+class Album(models.Model):
+	artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
+	name = models.CharField(max_length=100)
+	release_date = models.DateField()
+	num_stars = models.IntegerField()
+
+class Blog(models.Model):
+	name = models.CharField(max_length=100)
+	tagline = models.TextField()
+
+	def __str__(self):
+		return self.name
+
+class Author(models.Model):
+	name = models.CharField(max_length=200)
+	email = models.EmailField()
+
+	def __str__(self):
+		return self.name
+
+class Entry(models.Model):
+	blog = models.ForeignKey(Blog)
+	headline = models.CharField(max_length=255)
+	body_text = models.TextField()
+	pub_date = models.DateField()
+	mod_date = models.DateField()
+	author = models.ManyToManyField(Author)
+	n_comments = models.IntegerField()
+	n_pingbacks = models.IntegerField()
+	rating = models.IntegerField()
+
+	def __str__(self):
+		return self.headline
+
+def hh()
+	Entry.objects.filter(headline__contains='')
